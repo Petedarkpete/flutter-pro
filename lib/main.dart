a44013ec34f3588b0885465883658fcd4907ec12
Peter Ndahi <pndahi@micro-capholdings.co.ke>
2023-11-29 10:36:47 +0300

    Initialize

---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Center(
+            child: Text("Start App"),
+          ),
+          backgroundColor: const Color.fromARGB(255, 206, 170, 72),
+        ),
+        body: const Center(
+            child: Image(
+          image: AssetImage('save2.png'),
+          width: 600.0,
+          height: 600.0,
+        )),
+        backgroundColor: const Color.fromARGB(255, 230, 217, 191),
+      ),
+    ),
+  );
+}
