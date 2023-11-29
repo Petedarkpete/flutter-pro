f43ac07a9fb7e02764fd155359bb7de189978ab8
Petedarkpete <peterndahi2018@gmail.com>
2023-11-29 13:01:08 +0300

    containers

--- lib/main.dart
+++ lib/main.dart
@@ -1,23 +1,37 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(
-    MaterialApp(
+  runApp(MyWidget());
+}
+
+class MyWidget extends StatelessWidget {
+  const MyWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
           title: const Center(
-            child: Text("Start Appsss"),
+            child: Text("Start Apps"),
           ),
           backgroundColor: const Color.fromARGB(255, 206, 170, 72),
         ),
-        body: const Center(
-            child: Image(
-          image: AssetImage('save2.png'),
-          width: 600.0,
-          height: 600.0,
-        )),
-        backgroundColor: const Color.fromARGB(255, 230, 217, 191),
+        body: Container(
+          child: const Text('If the widget has an alignment, '
+              'and the parent provides bounded constraints, then the Container tries to expand to'
+              'fit the parent, and then positions the child within itself as per the alignment.'),
+          color: const Color.fromARGB(255, 230, 217, 191),
+          padding: const EdgeInsets.all(20.20),
+          margin: const EdgeInsets.all(20.20),
+          alignment: Alignment.bottomRight,
+          width: 200,
+          height: 100,
+        ),
+        backgroundColor: const Color.fromARGB(255, 252, 252, 252),
       ),
-    ),
-  );
+    );
+
+    return const Placeholder();
+  }
 }
